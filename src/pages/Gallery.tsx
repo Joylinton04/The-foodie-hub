0cc7ce2226fb6f00e1ded4f59e1fa8a46186ebf0
Joylinton04 <adujoy05@gmail.com>
2024-11-14 21:53:52 +0000

    gallery page header

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,8 +1,11 @@
 import Gall from "@/components/Gallery";
+import Header from "@/components/Header";
 
 const Gallery = () => {
   return (
     <div>
+        <Header title="Gallery" />
+      {/* section 1 */}
       <Gall/>
     </div>
   )
